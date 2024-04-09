62645a94269d5f0f3fc2d220b80239763d9398ab
bre5da <158521516+bre5da@users.noreply.github.com>
2024-04-09 21:04:08 +0000

    Javascript

--- variables.js
+++ variables.js
@@ -1,11 +1,11 @@
 // CODE ALONG, PART 1
-let favoriteColor;
+let favoriteColor = "Black"
 
 // 1. Create a variable called `favoriteFood`.
-
+let favoriteFood = "Quesadillas"
 
 // 2. Create a variable called `favoriteNumber`.
-
+let favoriteNumber = 25;
 
 
 
@@ -16,3 +16,7 @@ let favoriteColor;
 // - Use a number value of your favorite number.
 
 // 4. Console log each variable on a new line.
+
+console.log(favoriteColor)
+console.log(favoriteFood)
+console.log(favoriteNumber)
